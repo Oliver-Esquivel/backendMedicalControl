6420a24ad6a83e041e3efbb4b25bfa19d4e23e60
Oliver-Esquivel <[oliveresquivel46@gmail.com]>
2023-12-28 16:26:50 -0600

    rutas de enfermedades

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { deleteUser, formData, getUser, getUserById, signIn, updateUser } from "../controllers/auth.controllers.js";
 import { dataClinic, deleteConsult, getConsult, getDataClinicById, updateConsult } from "../controllers/authForm.controllers.js";
-import { addIllness } from "../controllers/authIllness.controllers.js";
+import { addIllness, dropIllness, getIllnessById } from "../controllers/authIllness.controllers.js";
 // import formLogin from "../controllers/auth.controllers.js";
-//que se ejecutaran al estar las respectivas rutas export the function 
+// export the function 
 const router = Router()
 
-//creamos las rutas y podemos acceder a las peticiones 
+//create de routes for access de petitions
 router.get('/' , getUser)       // --> Show data
 router.get('/:userId', getUserById) // --> search user id in bd
 router.post('/', formData) // --> create new register
@@ -15,13 +15,18 @@ router.put('/:userId', updateUser) //  --> modify the register
 router.delete('/:userId', deleteUser) // --> eliminate the user
 
 
-router.get('/clinic/:patientId', getConsult);    // -->not function  Obtener datos de la clínica
-router.get('/clinic/:userId', getDataClinicById); // -->yes function  Obtener datos de un paciente por ID
-router.post('/clinic', dataClinic);   // --> Insert data exam Medic funciona
+router.get('/clinic/:patientId', getConsult);    // -->not function  
+router.get('/clinic/:userId', getDataClinicById); // -->yes function 
+router.post('/clinic', dataClinic);   // --> Insert data exam Medic 
 router.put('/clinic/:patientId', updateConsult) //--> yes function 
 router.delete('/clinic/:patientId', deleteConsult) // --> yes function eliminate consult  
 
-//  **************************************************************
-
+//***********************ILLNESS******************************
+router.get('/illness/:illnessId', getIllnessById)
 router.post('/illness', addIllness)
+router.delete('/illness/:illnessId', dropIllness)
+
+//***********************************************************
+
+
 export default router
