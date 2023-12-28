b584769f71063fd604ff74f6309bbe70cd4c3213
Oliver-Esquivel <[oliveresquivel46@gmail.com]>
2023-12-28 16:26:18 -0600

    Modelo de base de datos enfermedades y exploracion fisica

--- src/models/formIllness.model.js
+++ src/models/formIllness.model.js
@@ -1,201 +1,71 @@
 import mongoose, { Schema } from "mongoose";
 
 const formIllness = new Schema({
-    ALLERGIC: {
-        type: Boolean
-    },
-    DIABETIC: {
-        type: Boolean
-    },
-    EPILEPTIC: {
-        type: Boolean
-    },
-    ARTHRITIC: {
-        type: Boolean
-    },
-    CARDIOVASCULAR: {
-        type: Boolean
-    },
-    HYPERTENSIVE: {
-        type: Boolean
-    },
-    HEMATOLOGIC: {
-        type: Boolean
-    },
-    TRAUMATIC: {
-        type: Boolean
-    },
-    PSYCHIATRIST: {
-        type: Boolean
-    },
-    NEOPLASTIC: {
-        type: Boolean
-    },
-    SURGICAL: {
-        type: Boolean
-    },
-    ENDOCRINOLOGIST: {
-        type: Boolean
-    },
-    GENITOURINARY: {
-        type: Boolean
-    },
-    NEUROLOGICAL: {
-        type: Boolean
-    },
-    ASTHMATIC: {
-        type: Boolean
-    },
-    ICTERICO: {
-        type: Boolean
-    },
-    CONGENITAL: {
-        type: Boolean
-    },
-    AUDIOLOGICAL: {
-        type: Boolean
-    },
-    TRANSFUSIONAL: {
-        type: Boolean
-    },
-    PARASITES: {
-        type: Boolean
-    },
-    OTHER: {
-        type: Boolean
-    },
-    MEASLES: {
-        type: Boolean
-    },
-    RUBELLA: {
-        type: Boolean
-    },
-    OTITIS: {
-        type: Boolean
-    },
-    CHICKENPOX: {
-        type: Boolean
-    },
-    SCARLETFEVER: {
-        type: Boolean
-    },
-    VENTS: {
-        type: Boolean
-    },
-    TUBERCULOSIS: {
-        type: Boolean
-    },
-    PAROTITIS: {
-        type: Boolean
-    },
-    TONSILLITIS: {
-        type: Boolean
-    },
-    BRONCHITIS: {
-        type: Boolean
-    },
-    OTHER: {
-        type: Boolean
-    },
-    MENARCHE: {
-        type: Boolean
-    },
-    RHYTHM: {
-        type: Boolean
-    },
-    MENSTRUALCYCLE: {
-        type: Boolean
-    },
-    FEATURES: {
-        type: Boolean
-    },
-    FUM: {
-        type: Boolean
-    },
-    HOME_ACTIVE_SEX_LIFE: {
-        type: Boolean
-    },
-    GESTATIONS: {
-        type: Boolean
-    },
-    DELIVERY: {
-        type: Boolean
-    },
-    CAESAREA: {
-        type: Boolean
-    },
-    ABORTION: {
-        type: Boolean
-    },
-    FERTILITY_CONTROL: {
-        type: Boolean
-    },
-    OTHER_ILLNESS: {
-        type: Boolean
-    },
-    INFECTIONS: {
-        type: Boolean
-    },
-    PROSTATE_DISEASE: {
-        type: Boolean
-    },
-    REPRODUCTIVE_DISORDERS: {
-        type: Boolean
-    },
-    TESTICULAR_PAIN: {
-        type: Boolean
-    },
-    OTHER: {
-        type: Boolean
-    },
-    FREQUENT_OPAQUE_URINATION: {
-        type: Boolean
-    },
-    MASSES_IN_TESTICLES: {
-        type: Boolean
-    },
-    VASECTOMY: {
-        type: Boolean
-    },
-    PEN_INFECTION: {
-        type: Boolean
-    },
-    PROSTATE_INFECTION: {
-        type: Boolean
-    },
-    TESTICULAR_INFECTION: {
-        type: Boolean
-    },
-    DIGESTIVE: {
-        type: Boolean
-    },
-    GENITOURINARY: {
-        type: Boolean
-    },
-    CARDIOVASCULAR: {
-        type: Boolean
-    },
-    RESPIRATORY: {
-        type: Boolean
-    },
-    NEUROLOGICAL: {
-        type: Boolean
-    },
-    SKINYTEGUMENTS: {
-        type: Boolean
-    },
-    ENDOCRINE: {
-        type: Boolean
-    },
-    HEMATOLOGIC: {
-        type: Boolean
-    },
-    SKELETAL_MUSCLE: {
-        type: Boolean
-    },
-    PSYCHIATRIST: {
-        type: Boolean
-    },
+    ALLERGIC: Boolean,
+    DIABETIC: Boolean,
+    EPILEPTIC: Boolean,
+    ARTHRITIC: Boolean,
+    CARDIOVASCULAR: Boolean,
+    HYPERTENSIVE: Boolean,
+    HEMATOLOGIC: Boolean,
+    TRAUMATIC: Boolean,
+    PSYCHIATRIST: Boolean,
+    NEOPLASTIC: Boolean,
+    SURGICAL: Boolean,
+    ENDOCRINOLOGIST: Boolean,
+    GENITOURINARY: Boolean,
+    NEUROLOGICAL: Boolean,
+    ASTHMATIC: Boolean,
+    ICTERICO: Boolean,
+    CONGENITAL: Boolean,
+    AUDIOLOGICAL: Boolean,
+    TRANSFUSIONAL: Boolean,
+    PARASITES: Boolean,
+    OTHER: Boolean,
+    MEASLES: Boolean,
+    RUBELLA: Boolean,
+    OTITIS: Boolean,
+    CHICKENPOX: Boolean,
+    SCARLETFEVER: Boolean,
+    VENTS: Boolean,
+    TUBERCULOSIS: Boolean,
+    PAROTITIS: Boolean,
+    TONSILLITIS: Boolean,
+    BRONCHITIS: Boolean,
+    OTHER: Boolean,
+    MENARCHE: Boolean,
+    RHYTHM: Boolean,
+    MENSTRUALCYCLE: Boolean,
+    FEATURES: Boolean,
+    FUM: Boolean,
+    HOME_ACTIVE_SEX_LIFE: Boolean,
+    GESTATIONS: Boolean,
+    DELIVERY: Boolean,
+    CAESAREA: Boolean,
+    ABORTION: Boolean,
+    FERTILITY_CONTROL: Boolean,
+    OTHER_ILLNESS: Boolean,
+    INFECTIONS: Boolean,
+    PROSTATE_DISEASE: Boolean,
+    REPRODUCTIVE_DISORDERS: Boolean,
+    TESTICULAR_PAIN: Boolean,
+    OTHER: Boolean,
+    FREQUENT_OPAQUE_URINATION: Boolean,
+    MASSES_IN_TESTICLES: Boolean,
+    VASECTOMY: Boolean,
+    PEN_INFECTION: Boolean,
+    PROSTATE_INFECTION: Boolean,
+    TESTICULAR_INFECTION: Boolean,
+    DIGESTIVE: Boolean,
+    GENITOURINARY: Boolean,
+    CARDIOVASCULAR: Boolean,
+    RESPIRATORY: Boolean,
+    NEUROLOGICAL: Boolean,
+    SKINYTEGUMENTS: Boolean,
+    ENDOCRINE: Boolean,
+    HEMATOLOGIC: Boolean,
+    SKELETAL_MUSCLE: Boolean,
+    PSYCHIATRIST: Boolean
 })
 export default mongoose.model('enfermedades', formIllness)
 
