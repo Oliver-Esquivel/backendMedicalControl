d8d82944e69235ab75ec372a5fc7d8763367c7cc
Oliver-Esquivel <[oliveresquivel46@gmail.com]>
2024-01-02 16:15:03 -0600

    Instalacion de libreria

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import morgan from 'morgan';
 import authRoute from './routes/auth.routes.js'
+import bodyParser from 'body-parser';
+import { validateDataUser } from './middlewares/authMiddleware.js';
+import { formData } from './controllers/auth.controllers.js';
+import { validateDataExamenMedico } from './middlewares/examenMedico.middleware.js';
+import { dataClinic } from './controllers/authForm.controllers.js';
 
 const app = express();
-
 app.use(morgan('dev')) // Moderatos the petitions 
 app.use(express.json())
 app.use('/api/auth', authRoute)
+
+app.use(bodyParser.json())
+// --> validate duplicate user data, 
+app.post('/api/auth/', validateDataUser, formData )
+// --> data require 
+app.post('/api/auth/clinic/', validateDataExamenMedico, dataClinic)
 
 export default app
 
