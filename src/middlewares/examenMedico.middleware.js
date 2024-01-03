b192698bb87466135b72a101931a661cf3a786ea
Oliver-Esquivel <[oliveresquivel46@gmail.com]>
2024-01-03 16:28:41 -0600

    requerimientos de datos antes de enviar

--- src/middlewares/examenMedico.middleware.js
+++ src/middlewares/examenMedico.middleware.js
@@ -2,17 +2,17 @@ import formDataExamenModel from "../models/formDataExamen.model.js";
 
 export const validateDataExamenMedico = async (req, res, next) => {
     try {
-        const { examen_medico, name_person, age, gender, turn, date_birth, plant, type_contract,
+        const { examen_medico, admission ,name_person, age, gender,status_Marital, turn, plant, type_contract,
             department, name_supervisor, plant_ant, position_work, domicile } = req.body;
 
             console.log('Request body', req.body)
-        if (!examen_medico || !name_person || !age || !gender || !turn || !date_birth || !plant ||
+            // --> we validate that the fields are filled 
+        if (!examen_medico || !admission  ||!name_person || !age || !gender || !status_Marital || !turn  || !plant ||
             !type_contract || !department || !name_supervisor || !plant_ant || !position_work || !domicile) {
             return res.status(400).json({ message: 'Data obligatory' });
         }
-
+            // -> checks if the user exists
         const existingUser = await formDataExamenModel.findOne({ name_person });
-
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
@@ -23,3 +23,4 @@ export const validateDataExamenMedico = async (req, res, next) => {
         res.status(500).json({ message: 'Error in server', error });
     }
 };
+
