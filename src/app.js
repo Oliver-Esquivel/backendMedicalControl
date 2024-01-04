877c14bba8f95bc1cca945170efae659a21a3708
Oliver-Esquivel <[oliveresquivel46@gmail.com]>
2024-01-04 16:21:29 -0600

    Configuracion de las rutas por archivo o importacion

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import morgan from 'morgan';
 import authRoute from './routes/auth.routes.js'
+import examRoute from './routes/examenMedico.routes.js'
+import illnessRoute from './routes/illness.routes.js'
+import physicalRoute from './routes/physical.routes.js'
+import laboratoryRoute from './routes/laboratory.routes.js'
 import bodyParser from 'body-parser';
 
 
@@ -8,6 +12,10 @@ const app = express();
 app.use(morgan('dev')) // Moderatos the petitions 
 app.use(express.json())
 app.use('/api/auth', authRoute)
+app.use('/api/auth/examenMedico', examRoute)
+app.use('/api/auth/illness', illnessRoute)
+app.use('/api/auth/physical', physicalRoute)
+app.use('/api/auth/laboratory', laboratoryRoute)
 
 app.use(bodyParser.json()) 
 
