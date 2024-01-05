0a69cd999e0222f94474921124fdbaee189e7cad
Oliver-Esquivel <[oliveresquivel46@gmail.com]>
2024-01-05 15:08:02 -0600

    uso de validaciones en las rutas de los controladores

--- src/routes/examenMedico.routes.js
+++ src/routes/examenMedico.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createExam, getExam } from "../controllers/authForm.controllers.js";
 import { authRequire } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { dataExams } from "../schemas/dataExams.schema.js";
 
 const examen = Router()
 
 examen.get('/',[authRequire],getExam)
 
-examen.post('/', [authRequire], createExam)
+examen.post('/', [authRequire], validateSchema(dataExams), createExam)
 
 export default examen
