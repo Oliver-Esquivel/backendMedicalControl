a1082369df48fa171c6ea623cd76ddea85631ff3
Oliver-Esquivel <[oliveresquivel46@gmail.com]>
2024-01-05 15:08:30 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 //routes
 import authRoute from './routes/auth.routes.js'
 import examRoute from './routes/examenMedico.routes.js'
@@ -10,7 +11,7 @@ import laboratoryRoute from './routes/laboratory.routes.js'
 
 
 const app = express();
-
+app.use(cors())
 app.use(morgan('dev')) // Moderatos the petitions 
 app.use(express.json())
 app.use(cookieParser())
